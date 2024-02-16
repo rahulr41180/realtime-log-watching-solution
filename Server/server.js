1bbf31531ac2a1777949170104d63dbb0cf1beb4
Rahul Rathor <www.rahulr41180@gmail.com>
2024-02-16 22:57:59 +0530

    server

--- Server/server.js
+++ Server/server.js
@@ -11,6 +11,8 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+app.use(cors());
+
 const io = new Server(server, {
   cors: {
     origin: ['http://localhost:3000', 'https://realtime-log-watching-solution-client.vercel.app'],
